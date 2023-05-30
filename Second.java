0ba542ba4824852cb639e18f492d6090e70c7750
shashank2791 <1234madhwal@gmail.com>
2023-05-30 17:00:54 +0530

    First Day Java Coding

---
+++ Second.java
@@ -0,0 +1,25 @@
+public class Second {
+    public static void main(String[] args) {
+        int num = 6;
+        int num2 = 9;
+        int res = num*num2;
+        System.out.println(res);
+    }
+}
+/*Data Types
+ *Primitive - 
+    Integer     
+                Int - 4 bytes
+                long - 8 bytes
+                short - 2 bytes
+                byte - 1 byte - -128 to 127
+
+    Float 
+                Float - 4 bytes
+                Double - 8 bytes
+
+    Character 
+                Char - 2 Bytes (UNICODE)
+
+    Bool        only True and False
+ */
