e89d1710f366c686b66460d09a6e96f6572ffd34
shashank2791 <1234madhwal@gmail.com>
2023-06-01 16:05:23 +0530

    Modified Loops.java

--- Loops.java
+++ Loops.java
@@ -1,5 +1,4 @@
 import java.util.Scanner;
-
 public class Loops {
     public static void main(String[] args) {
         try(Scanner sc = new Scanner(System.in)){
@@ -8,8 +7,14 @@ public class Loops {
             for(int i=0;i<count;i++){
                 System.out.println(i);
             }
+
+            do{
+                if(count == 0) System.out.println("Hello");
+                count--;
+            }while(count >= 0);
         }
         System.out.println("Enjoying JAVA coding");
+
     }
     
 }
