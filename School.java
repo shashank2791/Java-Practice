0f18052b3a25fd596c185ef8e9d134e075c18fc0
shashank2791 <1234madhwal@gmail.com>
2023-06-08 14:30:25 +0530

    School.java fixed now running as expected

--- School.java
+++ School.java
@@ -1,35 +1,28 @@
-import java.util.Scanner;
+import java.util.*;
 
 class students{
+    
+    static Scanner sc = new Scanner(System.in);
     static int count = 0;
-    Scanner sc = new Scanner(System.in);
     int roll_number;
     String name;
     char class_name;
-
-
-    public void get_student_details(){
-        int roll;
-        String name;
-        char class_name;
-        System.out.println("Enter student roll no.: ");
-        roll = sc.nextInt();
-        System.out.println("Enter student name: ");
-        name = sc.nextLine();
-        System.out.println("Enter student class: ");
-        class_name = sc.next().charAt(0);
-        this.roll_number = roll;
-        this.name = name;
-        this.class_name = class_name; 
+    public void get_students(){
+            System.out.println("Enter roll number: ");
+            this.roll_number = sc.nextInt();
+            sc.nextLine();
+            System.out.println("Enter name: ");
+            this.name = sc.nextLine();
+            System.out.println("Enter class: ");
+            this.class_name = sc.next().charAt(0);
     }
     public static void put_student_details(students s){
         System.out.println("Student with roll number " + s.roll_number + " and with name " + s.name + " studies in class " + s.class_name);
     }
     public static int get_count(){
-        try (Scanner sc = new Scanner(System.in)) {
             System.out.println("Enter number of Students you want to add: ");
             students.count = sc.nextInt();
-        }
+        
         return students.count;
     }
 }
@@ -37,8 +30,9 @@ public class School {
     public static void main(String[] args) {
         int count_students = students.get_count();
         students s [] = new students[count_students];
-        for(students t : s){
-            t.get_student_details();
+        for(int i = 0; i < count_students; i++){
+            s[i] = new students();
+            s[i].get_students();
         }
         for(students t: s){
             students.put_student_details(t);
